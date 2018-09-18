38279bb2e19d7d931fa0f62738638b81df5923ed
Pham Ha <phammanhha305@gmail.com>
2018-09-18 19:14:00 +0700

    18/09/2018 2
    thêm mới được sản phẩm

--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -9,10 +9,16 @@ $(function(){
 	$('#save-modal').click(function(){
 		// count = count + 1;
 		var elements = $('tbody>tr[data-key]');
-		var element = $('tbody>tr[data-key]:first').clone();
+		var element = $('tbody>tr[data-key]:last').clone();
 		element.attr('data-key', elements.length);
-		element.children('[data-col-seq]:first').html()
-		console.log(element.children('[data-col-seq]:first').html());
+		element.attr('id', 'stt_'+elements.length);
+		var stt = parseInt(element.children('[data-col-seq]:first').html());
+		stt++;
+		element.children('[data-col-seq]:first').html(stt);
+
+		var tensanpham = $('#donhangchitiet-idsanpham');
+		console.log(tensanpham);
+		
 
 		$('tbody').append(element);
 
